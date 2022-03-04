97415ad19964b720ad992ed76ee054fa24942b7d
zaiolul <zaiowrk321@gmail.com>
2022-03-04 22:08:28 +0200

    movement

--- Player.gd
+++ Player.gd
@@ -1,14 +1,18 @@
 extends KinematicBody2D
 
 var speed = 100
-var gravity = 40
+var gravity = 10
 
 var input = Vector2.ZERO
 var facing_direction = 1
 var velocity = Vector2.ZERO
 
 var jump_force = -200
+var jump_start_time = 0
+var jump_hold_delay = 100
+var jump_count = 2
 var pressed_jump = false
+var released_jump = false
 
 onready var main_sprite = $MainSprite
 
@@ -16,15 +20,36 @@ func _ready():
 	pass 
 	
 func _check_jump():
-	pressed_jump = Input.is_action_just_pressed("jump")
-	
-	if pressed_jump:
+	if Input.is_action_just_pressed("jump"):
+		pressed_jump = true
+		jump_start_time = OS.get_ticks_msec()
 		
-		velocity.y += jump_force
-		pressed_jump = false
+	if Input.is_action_just_released("jump") and velocity.y < jump_force / 2:
+		velocity.y = jump_force / 2
+		
+	if is_on_floor():
+		jump_count = 2
+		
+	if pressed_jump:
+		if is_on_floor():
+			velocity.y = jump_force
+			pressed_jump = false
+		else:
+			if jump_count >= 1:
+				velocity.y = jump_force
+				jump_count -= 1
+		if OS.get_ticks_msec() >= jump_start_time + jump_hold_delay:
+			
+			pressed_jump = false
+		else:
+			if is_on_floor():
+				velocity.y = jump_force
+				pressed_jump = false
+		
+		
 		
 func _physics_process(delta):
-	print(velocity)
+	print(jump_start_time, " ", OS.get_ticks_msec())
 	
 	input.x = Input.get_action_strength("right") - Input.get_action_strength("left")
 	
@@ -35,9 +60,9 @@ func _physics_process(delta):
 	if is_on_floor():
 		velocity.y = gravity
 	velocity.y += gravity
-		
-	velocity = move_and_slide(velocity, Vector2.UP)
 	_check_jump()
+	velocity = move_and_slide(velocity, Vector2.UP)
+	
 
 #func _process(delta):
 #	pass
