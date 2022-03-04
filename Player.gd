edf8e4cc5f9614bb28fea0e2228654003a816ff9
zaiolul <zaiowrk321@gmail.com>
2022-03-04 21:42:11 +0200

    new

--- Player.gd
+++ Player.gd
@@ -1,7 +1,7 @@
 extends KinematicBody2D
 
 var speed = 100
-var gravity = 100
+var gravity = 40
 
 var input = Vector2.ZERO
 var facing_direction = 1
@@ -19,18 +19,25 @@ func _check_jump():
 	pressed_jump = Input.is_action_just_pressed("jump")
 	
 	if pressed_jump:
-		velocity.y = jump_force
+		
+		velocity.y += jump_force
+		pressed_jump = false
 		
 func _physics_process(delta):
-	#velocity.y += gravity
+	print(velocity)
+	
 	input.x = Input.get_action_strength("right") - Input.get_action_strength("left")
 	
 	if input.x != 0:
 		velocity.x = input.x * speed
 	else:
 		velocity.x = 0
+	if is_on_floor():
+		velocity.y = gravity
+	velocity.y += gravity
 		
 	velocity = move_and_slide(velocity, Vector2.UP)
+	_check_jump()
 
 #func _process(delta):
 #	pass
