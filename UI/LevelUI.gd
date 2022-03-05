fdfeb2b79947a92cc8d6b02813f495f2fc07bf4c
Rokas Puzonas <rokas.puz@gmail.com>
2022-03-05 17:26:21 +0200

    feat: contrain movement of units to available tiles

---
+++ UI/LevelUI.gd
@@ -0,0 +1,13 @@
+extends Control
+
+signal pick_mutation;
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+
+func _on_MutationPicker_pick_mutation(mutation):
+	emit_signal("pick_mutation", mutation)
--- World.gd
+++ World.gd
@@ -38,33 +38,50 @@ func get_tile_position():
 	var pos = get_raycast_position()
 	if pos:
 		return $GridMap.world_to_map(pos)
-		
-func is_blocked(pos: Vector3):
-	var cell = $GridMap.get_cell_item(pos.x, pos.y, pos.z);
+
+func is_blocked(x, y):
+	return $GridMap.get_cell_item(x, 0, y) == -1 || \
+			$GridMap.get_cell_item(x, 1, y) == 3 || \
+			get_unit_at(x, y) != null
 
 func get_unit_at(x: int, y: int):
 	for unit in $PlayerUnits.get_children():
-
 		if unit.translation.x-0.5 == x && unit.translation.z-0.5 == y:
 			return unit
 
 func select_unit(unit):
 	selected_unit = unit
 	var Marker = load("res://MovementMarker.tscn")
-	available_moves = unit.get_move_tiles()
-	$MoveMarkers.translation = unit.translation - Vector3(1, -1, -1)
+
+	available_moves = []
+	var map_pos = $GridMap.world_to_map(unit.translation)
+	for tile in get_available_movement_tiles(map_pos.x, map_pos.z, unit.movement_range, unit.movement_pattern):
+		available_moves.push_front(tile)
+
+	$MoveMarkers.translation = Vector3(0.5, 0.5, 0.5)
 	for tile in available_moves:
 		var marker = Marker.instance()
 		marker.translation = Vector3(tile.x, 0, tile.y)
 		$MoveMarkers.add_child(marker)
+
+func move_selected(pos):
+	if !available_moves.has(Vector2(pos.x, pos.z)):
+		return false
+		
+	selected_unit.translation = Vector3(0.5+pos.x, 0.5, 0.5+pos.z)
+	selected_unit.moved = true
+	deselect_unit()
+	return true
 
 func deselect_unit():
 	selected_unit = null
 	for marker in $MoveMarkers.get_children():
 		$MoveMarkers.remove_child(marker)
 
-
 func _process(delta):
+	if Input.is_action_just_pressed("ui_cancel"):
+		deselect_unit()
+
 	var pos = get_tile_position();
 	if pos != null:
 		var unit_on_mouse = get_unit_at(pos.x, pos.z)
@@ -72,19 +89,18 @@ func _process(delta):
 			if selected_unit == null && unit_on_mouse != null and not unit_on_mouse.enemy and not unit_on_mouse.moved:
 				select_unit(unit_on_mouse)
 			elif selected_unit != null:
-				selected_unit.translation = Vector3(0.5+pos.x, 0.5, 0.5+pos.z)
-				selected_unit.moved = true
-				deselect_unit()
-				
-				
+				if !move_selected(pos):
+					deselect_unit()
+
 		show_marker_at(pos.x, pos.z)
 	else:
 		hide_marker()
+
 	if not end_turn:
 		if current_turn == TURN.player:
 			_reset_moved("EnemyUnits")
 			_check_moves("PlayerUnits", TURN.enemy)
-			
+
 		elif current_turn == TURN.enemy:
 			_reset_moved("PlayerUnits")
 			_enemy_move()
@@ -93,18 +109,19 @@ func _process(delta):
 		_reset_moved("PlayerUnits")
 		_enemy_move()
 		end_turn = false
-	
+
 func _enemy_move():
 	var units = get_node("EnemyUnits").get_children()
 
-	
 	for unit in units:
 		timer.start()
-		yield(timer, "timeout")
-		unit.translation = Vector3(0.5+unit.translation.x, 0.5, 0.5+unit.translation.z)
-		
-	
+		var map_pos = $GridMap.world_to_map(unit.translation)
+		var available_tiles = get_available_movement_tiles(map_pos.x, map_pos.z, unit.movement_range, unit.movement_pattern)
+		var tile = available_tiles[randi() % available_tiles.size()]
+		unit.translation = Vector3(tile.x+0.5, 0.5, tile.y+0.5)
 		unit.moved = true
+		yield(timer, "timeout")
+
 func _reset_moved(node):
 	var units = get_node(node).get_children()
 	for unit in units:
@@ -118,4 +135,67 @@ func _check_moves(node, next_turn):
 			moved_count += 1
 	if len(units) == moved_count:
 		current_turn = next_turn
-	
+
+
+func get_available_movement_tiles(x: int, y: int, move_range: int, move_pattern):
+	var all_tiles = []
+
+	for neigbour in get_neighours(x, y, move_pattern):
+		if is_blocked(neigbour.x, neigbour.y):
+			continue
+		if move_range > 1:
+			for sub_neigbour in get_available_movement_tiles(neigbour.x, neigbour.y, move_range-1, move_pattern):
+				if !all_tiles.has(sub_neigbour):
+					all_tiles.append(sub_neigbour)
+		if !all_tiles.has(neigbour):
+			all_tiles.append(neigbour)
+
+	if all_tiles.has(Vector2(x, y)):
+		all_tiles.remove(all_tiles.find(Vector2(x, y)))
+
+	return all_tiles
+
+func get_neighours(x: int, y: int, pattern):
+	match pattern:
+		MovementPattern.PATTERN.NORMAL:
+			return get_normal_neighbours(x, y)
+		MovementPattern.PATTERN.DIAGONAL:
+			return get_diagonal_neighbours(x, y)
+		MovementPattern.PATTERN.HORSE:
+			return get_horse_neighbours(x, y)
+		MovementPattern.PATTERN.LEFT_FORWARD:
+			return get_left_forward_neighbours(x, y)
+
+func get_normal_neighbours(x: int, y: int):
+	return [
+		Vector2(x-1, y),
+		Vector2(x, y-1),
+		Vector2(x, y+1),
+		Vector2(x+1, y)
+	]
+
+func get_left_forward_neighbours(x: int, y: int):
+	return [
+		Vector2(x-1, y),
+		Vector2(x, y-1)
+	]
+
+func get_diagonal_neighbours(x, y):
+	return [
+		Vector2(x-1, y-1),
+		Vector2(x+1, y-1),
+		Vector2(x+1, y+1),
+		Vector2(x-1, y+1)
+	]
+
+func get_horse_neighbours(x, y):
+	return [
+		Vector2(x-1, y-2),
+		Vector2(x-2, y-1),
+		Vector2(x-1, y+2),
+		Vector2(x-2, y+1),
+		Vector2(x+1, y-2),
+		Vector2(x+2, y-1),
+		Vector2(x+2, y+1),
+		Vector2(x+1, y+2),
+	]
