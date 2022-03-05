1b4c9733a5d6858f5a505d9516121fba5d45ea20
Rokas Puzonas <rokas.puz@gmail.com>
2022-03-05 02:14:48 +0200

    feat: working camera raycast with grid map

--- Camera.gd
+++ Camera.gd
@@ -1,20 +1,19 @@
 extends Camera
 
-
-
-var mouse_pos: Vector2
-
 signal cell_pressed
 
 func get_cell_pos():
-	mouse_pos = get_viewport().get_mouse_position()
+	var mouse_pos = get_viewport().get_mouse_position()
 	var space_state = get_world().direct_space_state
 	var from = project_ray_origin(mouse_pos)
 	var to = from + project_ray_normal(mouse_pos) * 10000
 	var cursorPos = space_state.intersect_ray(from, to)
+
 	return cursorPos.get("position")
+
 func _physics_process(delta):
 	if Input.is_action_just_pressed("mouse_press"):
+		print(get_cell_pos())
 		emit_signal("cell_pressed", get_cell_pos())
 	
 
