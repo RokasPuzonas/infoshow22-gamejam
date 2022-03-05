db7bfbcd5824c2354c1460f0d6abffd560870624
Rokas Puzonas <rokas.puz@gmail.com>
2022-03-05 23:44:59 +0200

    feat: add background music

--- SetMenuPauseMode.gd
+++ SetMenuPauseMode.gd
@@ -3,3 +3,6 @@ extends Control
 
 func _ready():
 	pause_mode =Node.PAUSE_MODE_PROCESS
+	
+	var music = load("res://BackgroundMusic.tscn").instance()
+	get_tree().get_root().call_deferred("add_child", music)
